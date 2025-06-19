a798478d83463adff06aa38800dfa2bc211aaea7
Guido Putignano <63144542+guidoputignano@users.noreply.github.com>
2025-06-19 09:55:43 +0200

    Update __init__.py

--- endothelial_simulation/visualization/__init__.py
+++ endothelial_simulation/visualization/__init__.py
@@ -3,6 +3,5 @@ Initialization file for visualization package.
 """
 
 from .plotters import Plotter
-from .enhanced_plotter import EnhancedPlotter
 
-__all__ = ['Plotter', 'EnhancedPlotter']
+__all__ = ['Plotter']
